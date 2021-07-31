3265c6e37df817d4dfadd4a55d8a98520ccad9b8
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-07-31 01:46:45 -0400

    done with lab

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using RatingAdjustment.Services;
 using BreadmakerReport.Models;
+using System.Collections.Generic;
 
 namespace BreadmakerReport
 {
@@ -15,17 +16,34 @@ namespace BreadmakerReport
         {
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
-            var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
-                .ToList();
+            var BMList = BreadmakerDb.Breadmakers;
+
+            var Alldata = (from data in BMList
+                           select new
+                           {
+                               Title = data.title,
+                               Reviews = (Double)data.Reviews.Count(),
+                               Average = (Double)BreadmakerDb.Reviews.
+                                   Where(row => row.BreadmakerId == data.BreadmakerId)
+                                   .Select(row => row.stars).Sum() / data.Reviews.Count(),
+                               Adjust = ratingAdjustmentService.Adjust(
+                                        (Double)BreadmakerDb.Reviews
+                                        .Where(row => row.BreadmakerId == data.BreadmakerId)
+                                        .Select(row => row.stars).Sum() / data.Reviews.Count()
+                                         ,
+                                        (Double)data.Reviews.Count())
+                           }).ToList().OrderByDescending(r => r.Adjust).ToList();
+            // TODO: add LINQ logic ...
+            //       ...
+            //.ToList();
 
             Console.WriteLine("[#]  Reviews Average  Adjust    Description");
             for (var j = 0; j < 3; j++)
             {
-                var i = BMList[j];
+                var i = Alldata[j];
                 // TODO: add output
                 // Console.WriteLine( ... );
+                Console.WriteLine($"[{j}] {i.Reviews, 7} {Math.Round(i.Average,2), -7} {Math.Round(i.Adjust, 2), -6} {i.Title}");
             }
         }
     }
